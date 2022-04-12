add8248ca6014eb9eaf44dd9279b6546cce0a19a
xavier <xavier@gmail.com>
2022-04-12 17:45:17 +0000

    commit extended code

--- multiple_calls_2.py
+++ multiple_calls_2.py
@@ -2,4 +2,6 @@ def func_e():
   print("we have given a partial fix")
   print("but we still want to do some more work on it")
 
+def func_f():
+  print("we add temporal changes that we do not commit")
 func_e()
